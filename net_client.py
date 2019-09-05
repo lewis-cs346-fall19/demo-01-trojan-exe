0c5769123bb76bb690da095fc15383811bdc1d7e
StarsGuardian <51521308+StarsGuardian@users.noreply.github.com>
2019-09-05 12:56:20 -0700

    Ready to be graded

--- net_client.py
+++ net_client.py
@@ -15,7 +15,8 @@ class Client:
 
     def generating_data(self):
         for i in range(1,10):
-            msg = input("Enter your message: ")
+            msg = str(i) + " egassem si sihT"
+            print("Message sent to Server: " + msg)
             self._sock.sendall(msg.encode())
             get = self._sock.recv(1024).decode()
             print(get)
